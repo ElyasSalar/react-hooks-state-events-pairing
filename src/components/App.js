c95c5863f52fb4a9ba878818568269fc2e685454
Elyas <elyassalar8@gmail.com>
2021-09-15 17:05:37 +0300

    65th assignment solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,52 @@
+import React, { useState } from 'react';
 import video from "../data/video.js";
 
 function App() {
-  console.log("Here's your data:", video);
+  const { comments, createdAt, downvotes, upvotes, embedUrl, title, views } = video;
+
+  const [isApeared, setIsApeared] = useState(true);
+  const [upvoteNum, setUpvoteNum] = useState(upvotes);
+  const [downvoteNum, setDownvoteNum] = useState(downvotes);
+
+  function handleCommentAppearance(){
+    setIsApeared(prev => !prev)
+  }
+
+  function handleUpvotes(){
+    setUpvoteNum(prev => prev + 1);
+  }
+
+  function handleDownvotes(){
+    setDownvoteNum(prev => prev - 1);
+  }
+
 
   return (
     <div className="App">
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={embedUrl}
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
       />
+      <h1>{title}</h1>
+      <p>{views} Views | Uploaded {createdAt}</p>
+      <button onClick={handleUpvotes}>{upvoteNum}👍</button>
+      <button onClick={handleDownvotes}>{downvoteNum}👎</button>
+      <br />
+      <button onClick={handleCommentAppearance}>{isApeared ? "Hide Comments" : "ShowComments"}</button>
+      <hr />
+      <h4>{comments.length} Comments</h4>
+      {
+        isApeared && comments.map(({comment, user, id}) => (
+          <li key={id} style={{listStyle: 'none'}}>
+            <h5>{user}</h5>
+            <p>{comment}</p>
+          </li>
+        ))
+      }
     </div>
   );
 }
